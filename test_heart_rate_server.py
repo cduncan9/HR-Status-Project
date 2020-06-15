db3e89449454f7e200469e0367ab7a5111afcbdd
Aidan <aidan.therien@duke.edu>
2020-06-15 15:07:32 -0400

    added method and unit testing for bad patient id and age input

--- test_heart_rate_server.py
+++ test_heart_rate_server.py
@@ -3,12 +3,28 @@ import numpy as np
 
 
 @pytest.mark.parametrize("data, expected",
+                         [(123, False),
+                          ("123", False),
+                          ("abc123", True)])
+def test_check_bad_input(data, expected):
+    from heart_rate_server import check_bad_input
+    answer = check_bad_input(data)
+    assert answer == expected
+
+
+@pytest.mark.parametrize("data, expected",
                          [({"patient_id": 1,
                             "attending_username": "Canyon.D",
                             "patient_age": 20}, [1, "Canyon.D", 20]),
                           ({"patient_id": 123,
                             "attending_username": "Aidan.T",
-                            "patient_age": 21}, [123, "Aidan.T", 21])])
+                            "patient_age": 21}, [123, "Aidan.T", 21]),
+                          ({"patient_id": "abc",
+                            "attending_username": "Canyon.D",
+                            "patient_age": 20}, "ERROR"),
+                          ({"patient_id": 5,
+                            "attending_username": "Canyon.D",
+                            "patient_age": "a20"}, "ERROR")])
 def test_read_patient(data, expected):
     from heart_rate_server import read_patient
     answer = read_patient(data)
