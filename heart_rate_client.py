cb2658c644ce998dd982e2ac91a19d6a871ae4b4
Canyon Duncan <canyon.duncan@duke.edu>
2020-06-16 19:04:09 -0400

    Added more post requests to the client module

--- heart_rate_client.py
+++ heart_rate_client.py
@@ -9,10 +9,20 @@ def add_new_patient():
                    "patient_age": 21}
     r = requests.post(server_name+"/api/new_patient", json=new_patient)
     print(r.text)
+    new_patient = {"patient_id": 2,
+                   "attending_username": "Therien.A",
+                   "patient_age": 21}
+    r = requests.post(server_name + "/api/new_patient", json=new_patient)
+    print(r.text)
+    new_patient = {"patient_id": 3,
+                   "attending_username": "Therien.A",
+                   "patient_age": 65}
+    r = requests.post(server_name + "/api/new_patient", json=new_patient)
+    print(r.text)
 
 
 def add_heart_rate():
-    new_heart_rate = {"patient_id": 1, "heart_rate": 200}
+    new_heart_rate = {"patient_id": 3, "heart_rate": 200}
     r = requests.post(server_name+"/api/heart_rate", json=new_heart_rate)
     print(r.text)
 
@@ -24,6 +34,13 @@ def add_new_attendant():
                      "patients": list()}
     r = requests.post(server_name+"/api/new_attending", json=new_attendant)
     print(r.text)
+    new_attendant = {"attending_username": "Therien.A",
+                     "attending_email": "a.therien@duke.edu",
+                     "attending_phone": "919-265-9874",
+                     "patients": list()}
+    r = requests.post(server_name + "/api/new_attending", json=new_attendant)
+    print(r.text)
+
 
 
 if __name__ == '__main__':
