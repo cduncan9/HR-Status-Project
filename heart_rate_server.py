b4c725daf888ee4b55c270a14311a29e616b525f
Aidan <aidan.therien@duke.edu>
2020-06-15 20:58:09 -0400

    wrote code to create attendant database and add new patient to attendant's list

--- heart_rate_server.py
+++ heart_rate_server.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 
 patient_db = list()
+attendant_db = list()
 
 app = Flask(__name__)
 
@@ -65,6 +66,25 @@ def add_patient_to_db(info):
     patient_db.append(new_patient_dict)
 
 
+def add_attendant_to_db(info):
+    new_attendant_dict = {"attending_username": info[0],
+                          "attending_email": info[1],
+                          "attending_phone": info[2],
+                          "patients": list()}
+    attendant_db.append(new_attendant_dict)
+
+
+def add_patient_to_attendant_db(info):
+    patient_id = info[0]
+    attendant_name = info[1]
+    for attendant in attendant_db:
+        if attendant["attending_username"] == attendant_name:
+            attendant["patients"].append(patient_id)
+            return False
+    return True
+
+
+
 @app.route("/api/new_patient", methods=["POST"])
 def post_new_patient():
     in_dict = request.get_json()
@@ -73,9 +93,18 @@ def post_new_patient():
         return verify_input, 400
     patient_info = read_patient(in_dict)
     add_patient_to_db(patient_info)
+    if add_patient_to_attendant_db(patient_info):
+        return "Attendant does not exist", 400
     print(patient_db)
     return "Patient information stored", 200
 
+
+@app.route("/api/new_attending", methods=["POST"])
+def post_new_attending():
+    in_dict = request.get_json()
+    add_patient_to_db(read_attending(in_dict))
+    print(attendant_db)
+    return "Attendant information stored", 200
 
 if __name__ == '__main__':
     app.run()
